cd1f593fbc87e1830a2ec560b623e31096da1a82
Junior774 <juniorcarpenedo123@gmail.com>
2021-05-30 09:08:50 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [tarefas, setTarefa] = useState([]); //tarefas é o array onde a lista será gravada, set é pra alterar ele.
+  //tarefas.push("oi");
+
+  const adicionarTarefas = (value) => {
+    setTarefa([...tarefas, value]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onSave={adicionarTarefas} />
+      <List tarefas={tarefas} />
+    </div>
+  );
+}
+
+export function Form(props) {
+  //const outraForma = props.onSave;
+  const [nome_tarefa, setText] = useState("");
+  return (
+    <div>
+      <input type="text" onChange={e => setText(e.currentTarget.value)} />
+      <button onClick={() => props.onSave(nome_tarefa)}>Salvar</button>
+    </div>
+  );
+}
+
+export function List(props) { //obs: de class a essa div q é criada.
+  const { tarefas } = props;
+  return (
+    <div>
+      {
+        tarefas.map(tarefa => {
+          return (
+            <div>
+              <span>{tarefa}</span>
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
