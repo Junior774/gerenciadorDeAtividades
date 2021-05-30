8a5ad107dc622f7906fd83bc69b6064054cccdf7
Junior774 <juniorcarpenedo123@gmail.com>
2021-05-30 11:03:20 -0300

    master

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-
+  const [id, incrementId] = useState(0);
   const [tarefas, setTarefa] = useState([]); //tarefas é o array onde a lista será gravada, set é pra alterar ele.
   //tarefas.push("oi");
 
-  const adicionarTarefas = (value) => {
-    setTarefa([...tarefas, value]);
+  const adicionarTarefas = ({ nome_tarefa, data_inicial }) => {
+    incrementId(id + 1);
+    setTarefa([...tarefas, { nome_tarefa, data_inicial, id }]);
   };
   return (
     <div className="App">
@@ -21,10 +22,17 @@ function App() {
 export function Form(props) {
   //const outraForma = props.onSave;
   const [nome_tarefa, setText] = useState("");
+  const [data_inicio, setDate] = useState();
+
   return (
     <div>
       <input type="text" onChange={e => setText(e.currentTarget.value)} />
-      <button onClick={() => props.onSave(nome_tarefa)}>Salvar</button>
+      <input type="date" onChange={e => setDate(e.currentTarget.value)} />
+      <button onClick={() => props.onSave({
+        nome_tarefa: nome_tarefa,
+        data_inicial: data_inicio,
+        id: Math.floor(Math.random() * 10000)
+      })}>Salvar</button>
     </div>
   );
 }
@@ -36,8 +44,8 @@ export function List(props) { //obs: de class a essa div q é criada.
       {
         tarefas.map(tarefa => {
           return (
-            <div>
-              <span>{tarefa}</span>
+            <div key={tarefa.id} >
+              <li>{tarefa.id + " " + tarefa.nome_tarefa + " " + tarefa.data_inicial}</li>
             </div>
           );
         })
